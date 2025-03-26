f38502bf70f34c16b059d36a0f04086e3db37826
bidhuripriyanshu <bidhuripriyanshu351@gmail.com>
2025-03-26 15:58:00 +0530

    add multer and cloudinary

--- middleware/multer.middleware.js
+++ middleware/multer.middleware.js
@@ -1,26 +1,14 @@
-const multer = require('multer');
-const path = require('path');
+import multer from "multer"; 
 
-// Configure storage
+// directly taking from the github page of the  multer docs
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './public/temp'); // Local folder for temporary storage
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname);
-    }
-});
-
-// File filter to accept only PDFs and DOCX files
-const fileFilter = (req, file, cb) => {
-    const allowedTypes = ['application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'];
-    if (allowedTypes.includes(file.mimetype)) {
-        cb(null, true);
-    } else {
-        cb(new Error('Only PDF and DOCX files are allowed!'), false);
-    }
-};
-
-const upload = multer({ storage, fileFilter });
-
-module.exports = upload;
+  destination: function (req, file, cb) {
+    cb(null, './public/temp')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+export const upload = multer({ 
+  storage
+})
--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,11 +1,34 @@
-const cloudinary = require('cloudinary').v2;
-require('dotenv').config(); // Load environment variables
+import{v2 as cloudinary} from 'cloudinary';
+import fs from'fs';
+    
+const uploadOnCloudinary = async(localFilePath) =>{
+    
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key:process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
 
-// Configure Cloudinary
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+    try{
+        if(!localFilePath) return null;
+        //uplaod the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"image",
+        })
+        fs.unlinkSync(localFilePath)
+        
+        //File has been uploaded successfully
+        console.log(
+            "file is uploaded on cloudinary",
+            response.url
+        );
+            return response;
+    }
+    catch(error){
+        fs.unlinkSync(localFilePath)  // remove the locally saved temporary file as the upload option got failed
+        console.log("Error While Uploading the File on Cloudinary,",error)
+        return null;
+    }
+}   
 
-module.exports = cloudinary;
+export{uploadOnCloudinary}
